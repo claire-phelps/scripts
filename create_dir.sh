6a89de5308f5f93bf52089ff816a2f8ebca178e5
Phelps <Claire.Phelps@bnymellon.com>
2021-08-18 12:06:18 -0400

    added new comment

--- create_dir.sh
+++ create_dir.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This script prompts for a directory name and creates it
 # in the current working directory.
+# New comment
 
 echo "Enter directory name you wish to create"
 read dirname
